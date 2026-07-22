b69cb65f34fe068ea7c3aa857695b9e9c6be7d60
Vitalii Verzun <vitaliy.vz.main@gmail.com>
2026-07-23 02:19:54 +0300

    configure nvim neo-tree plugin

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,71 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    lazy = false,
+    priority = 1000,
+    init = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function(data)
+                local is_dir = vim.fn.isdirectory(data.file) == 1
+                if is_dir then
+                    require("neo-tree.command").execute({ action = "focus", dir = data.file })
+                end
+            end,
+        })
+    end,
+    opts = {
+        filesystem = {
+            group_empty_dirs = true, -- Collapses chains like src/main/java/ into one line
+            scan_mode = "deep", -- Scans deep so nested empty folders collapse/expand instantly
+            bind_to_cwd = true,
+            follow_current_file = {
+                enabled = true,
+                leave_dirs_open = false,
+            },
+            hijack_netrw_behavior = "disabled",
+            use_libuv_file_watcher = true,
+            filtered_items = {
+                visible = false,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+            window = {
+                mappings = {
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root",
+                    ["h"] = "close_node",
+                    ["l"] = "open",
+                },
+            },
+        },
+        window = { position = "left", width = 35 },
+        default_component_configs = {
+            git_status = {
+                symbols = {
+                    added = "✚",
+                    modified = "",
+                    deleted = "✖",
+                    renamed = "󰁕",
+                    untracked = "",
+                    ignored = "",
+                    unstaged = "󰄱",
+                    staged = "",
+                    conflict = "",
+                },
+            },
+            diagnostics = {
+                symbols = {
+                    hint = "󰌵 ",
+                    info = " ",
+                    warn = " ",
+                    error = " ",
+                },
+            },
+        },
+    },
+    keys = {
+        { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer (Neo-tree)" },
+    },
+}
